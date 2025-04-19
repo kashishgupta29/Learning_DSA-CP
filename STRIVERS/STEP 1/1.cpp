c6199fb3d6a077e7ed9e933186d53c72c1fe315e
kashishgupta29 <119280832+kashishgupta29@users.noreply.github.com>
2025-04-19 23:09:37 +0530

    Your update message

--- STRIVERS/STEP 1/1.cpp
+++ STRIVERS/STEP 1/1.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 void pattern(int &n)
 {
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      cout << "*" << " ";
+    }
+    cout << endl;
+  }
 }
 int main()
 {
